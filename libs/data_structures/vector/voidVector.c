f65176a5f7b3fdbbb2d4562222a20ea7271cdb45
vladbelpv232 <vladbeloborodov@gmail.com>
2024-04-29 15:07:14 +0300

    memory usage of vectorVoid

--- libs/data_structures/vector/voidVector.c
+++ libs/data_structures/vector/voidVector.c
@@ -15,3 +15,32 @@ vectorVoid createVectorV(size_t n, size_t baseTypeSize) {
     return result;
 }
 
+void reserveV(vectorVoid *v, const size_t newCapacity) {
+    if (newCapacity == 0)
+        v->data = NULL;
+    else
+        v->data = (int *) realloc(v->data, v->baseTypeSize *
+                                           newCapacity);
+    if (v->data == NULL) {
+        fprintf(stderr, "bad alloc");
+        exit(1);
+    }
+    if (v->size > newCapacity)
+        v->size = newCapacity;
+    v->capacity = newCapacity;
+}
+
+
+void shrinkToFitV(vectorVoid *v) {
+    reserveV(v, v->size);
+}
+
+
+void clearV(vectorVoid *v) {
+    v->size = 0;
+}
+
+
+void deleteVectorV(vectorVoid *v) {
+    free(v->data);
+}
--- libs/data_structures/vector/voidVector.h
+++ libs/data_structures/vector/voidVector.h
@@ -17,6 +17,17 @@ typedef struct vectorVoid {
 //возвращает структуру-дескриптор вектор из n значений.
 vectorVoid createVectorV(size_t n, size_t baseTypeSize);
 
+//изменяет количество памяти, выделенное под хранение элементов вектора.
+void reserveV(vectorVoid *v, size_t newCapacity);
+
+//удаляет элементы из контейнера, но не освобождает выделенную память.
+void shrinkToFitV(vectorVoid *v);
+
+//освобождает память, выделенную поднеиспользуемые элементы.
+void clearV(vectorVoid *v);
+
+//освобождает память, выделенную вектору.
+void deleteVectorV(vectorVoid *v);
 
 
 #endif
