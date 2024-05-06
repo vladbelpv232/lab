cb6dc59b25d32d2c64076794e2df4d56b6b218dd
vladbelpv232 <vladbeloborodov@gmail.com>
2024-05-06 13:13:07 +0300

    add func for testing matrix/ add tests_for_matrix/refactoring/ bug fix

--- libs/data_structures/matrix/matrix.c
+++ libs/data_structures/matrix/matrix.c
@@ -1,36 +1,37 @@
-#include <stdlib.h>
-#include <malloc.h>
-#include <stdio.h>
-#include <stdbool.h>
 #include "matrix.h"
 #include <assert.h>
-#include <math.h>
 
+
+//размещает в динамической памяти матрицу размером nRows на nCols
 matrix getMemMatrix (int nRows, int nCols) {
     int **values = (int **) malloc(sizeof(int*) * nRows);
     for (int i = 0; i < nRows; i++) {
         values[i] = (int *) malloc(sizeof(int) * nCols);
-    }
 
     return (matrix){values, nRows, nCols};
 }
 
+//размещает в динамической памяти массив из nMatrices матриц размером nRows на nCols.
 matrix *getMemArrayOfMatrices(int nMatrices,int nRows, int nCols) {
     matrix *ms = (matrix*) malloc(sizeof(matrix) * nMatrices);
-    for (int i = 0; i < nMatrices; i++)
+    for (int i = 0; i < nMatrices; i++) {
         ms[i] = getMemMatrix(nRows, nCols);
+    }
     return ms;
 }
-
+//освобождает память, выделенную под хранение матрицы m.
 void freeMemMatrix(matrix *m) {
     free(m->values);
 }
 
+//освобождает память, выделенную под хранение массива ms из nMatrices матриц.
 void freeMemMatrices(matrix *ms, int nMatrices) {
     for (int i = 0; i < nMatrices; i++) {
         free(ms[i].values);
     }
 }
+
+    //ввод матрицы m.
 void inputMatrix(matrix *m) {
     for (int i = 0; i < m->nRows; i++) {
         for (int j = 0; j < m->nCols; j++)
@@ -39,12 +40,14 @@ void inputMatrix(matrix *m) {
     }
 }
 
+    //ввод массива из nMatrices матриц, хранящейся по адресу ms.
 void inputMatrices(matrix *ms, int nMatrices) {
     for (int k = 0; k < nMatrices; k++) {
         inputMatrix(&ms[k]);
     }
 }
 
+// вывод матрицы m.
 void outputMatrix(matrix m) {
     for (int i = 0; i < m.nRows; i++) {
         for (int j = 0; j < m.nCols; j++)
@@ -53,74 +56,90 @@ void outputMatrix(matrix m) {
     }
 }
 
+//вывод массива из nMatrices матриц, хранящейся по адресу ms.
 void outputMatrices(matrix *ms, int nMatrices) {
     for (int i = 0; i < nMatrices; i++) {
         outputMatrix(ms[i]);
     }
 }
 
-void swap_pointers(int **a, int **b) {
-    int *t = *a;
-    *a = *b;
-    *b = t;
-}
-
+// обмен строк с порядковыми номерами i1 и i2 в матрице m.
 void swapRows(matrix m, int i1, int i2) {
     assert(0 <= i1 && i1 < m.nRows);
     assert(0 <= i2 && i2 < m.nRows);
-    swap_pointers(&m.values[i1], &m.values[i2]);
+    *b = t
 }
 
-void swap(int *a, int *b) {
-    int t = *a;
-    *a = *b;
-    *b = t;
-}
 
+//обмен колонок с порядковыми номерами j1 и j2 в матрице m.
 void swapColumns(matrix m, int j1, int j2) {
     assert(0 <= j1 && j1 < m.nCols);
     assert(0 <= j2 && j2 < m.nCols);
-    swap_pointers(&m.values[j1], &m.values[j2]);
-    swap(&m.values[j1], &m.values[j2]);
+    for (int i = 0; i < m.nRows; i++)
+        swap(&m.values[i][j1], &m.values[i][j2]);
 }
 
-void insertionSortRowsMatrixByRowCriteria(matrix m, int (*criteria)(int*, int)) {
-    int *criteriaValues = (int*)malloc(sizeof(int) * m.nRows);
-    for (int i = 0; i < m.nRows; i++)
-        criteriaValues[i] = criteria(m.values[i], m.nCols);
-    for (int i = 0; i < m.nRows; i++) {
-        int minIndex = i;
+// функция-критерий для сравнения строк по сумме элементов
+    int getSum(int* a, int nCols) {
+        int sum = 0;
+        for (int i = 0; i < nCols; i++) {
+            sum += a[i];
+        }
+
+        return sum;
+    }
+
+// функция-критерий для сравнения строк по максимальному элементу
+    int getMax(int* a, int n) {
+        assert(n > 0);
+        int max = a[0];
+        for (int i = 1; i < n; i++) {
+            if (a[i] > max)
+                max = a[i];
+        }
+        return max;
+    }
+
+
+// выполняет сортировку вставками строк
+//матрицы m по неубыванию значения функции criteria применяемой для строк
+    void insertionSortRowsMatrixByRowCriteria(matrix m, int (*criteria)(int*, int)) {
+        int *criteriaValues = (int *) malloc(sizeof(int) * m.nRows);
+        for (int i = 0; i < m.nRows; i++)
+            criteriaValues[i] = criteria(m.values[i], m.nCols);
+        for (int i = 0; i < m.nRows; i++) {
+            int minIndex = i;
+        }
         for (int j = i + 1; j < m.nRows; j++)
-            if (criteriaValues[j] < criteriaValues[minIndex])
+            if (criteriaValues[j] > criteriaValues[minIndex])
                 minIndex = j;
         if (i != minIndex) {
             swap_pointers(&criteriaValues[i], &criteriaValues[minIndex]);
-            swapRows(m, i, minIndex);
         }
+        free(criteriaValues);
     }
-    free(criteriaValues);
-}
 
-void selectionSortColsMatrixByColCriteria(matrix m, int (*criteria)(int*, int)) {
-    int *criteriaValues = (int*)malloc(sizeof(int) * m.nCols);
-    int *column = (int*)malloc(sizeof(int) * m.nRows);
-    for (int j = 0; j < m.nCols; j++) {
-        for (int i = 0; i < m.nRows; i++) {
-            column[i] = m.values[i][j];
+//выполняет сортировку выбором столбцов
+//матрицы m по неубыванию значения функции criteria применяемой для столбцов
+    void selectionSortColsMatrixByColCriteria(matrix a, int (*criteria)(int*, int)) {
+        int *criteriaValues = (int*)malloc(sizeof(int) * a.nCols);
+        int *column = (int*)malloc(sizeof(int) * a.nRows);
+        for (int j = 0; j < a.nCols; j++) {
+            for (int i = 0; i < a.nRows; i++)
+                column[i] = a.values[i][j];
+            criteriaValues[j] = criteria(column, a.nCols);
         }
-        criteriaValues[j] = criteria(column, m.nCols);
-    }
 
     for (int i = 0; i < m.nCols; i++) {
         int minIndex = i;
-        for (int j = i + 1; j < m.nCols; j++) {
+        for (int j = i + 1; j < a.nCols; j++) {
             if (criteriaValues[j] < criteriaValues[minIndex]) {
                 minIndex = j;
             }
         }
         if (i != minIndex) {
             swap(&criteriaValues[i], &criteriaValues[minIndex]);
-            swapColumns(m, i, minIndex);
+            swapColumns(a, i, minIndex);
         }
     }
     free(column);
@@ -230,3 +249,29 @@ position getMaxValuePos(matrix m){
     }
     return maxPos;
 }
+// возвращает матрицу размера nRows на nCols, построенную из элементов массива a
+    matrix createMatrixFromArray(const int *a, size_t nRows, size_t nCols) {
+        matrix m = getMemMatrix(nRows, nCols);
+        int k = 0;
+
+        for (int i = 0; i < nRows; i++)
+            for (int j = 0; j < nCols; j++)
+                m.values[i][j] = a[k++];
+
+        return m;
+    }
+
+//возвращает указатель на нулевую матрицу массива из nMatrices матриц, размещенных
+//в динамической памяти, построенных из элементов массива a
+    matrix *createArrayOfMatrixFromArray(const int *values, size_t nMatrices, size_t nRows, size_t nCols) {
+        matrix *ms = getMemArrayOfMatrices(nMatrices, nRows, nCols);
+        int l = 0;
+        for (size_t k = 0; k < nMatrices; k++)
+            for (size_t i = 0; i < nRows; i++)
+                for (size_t j = 0; j < nCols; j++)
+                    ms[k].values[i][j] = values[l++];
+
+        return ms;
+    }
+
+
