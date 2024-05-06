5f8f0d1ed252a8e3a2b388e1164d3ae5ac681dd3
vladbelpv232 <vladbeloborodov@gmail.com>
2024-05-06 11:30:57 +0300

    init Matrix and position

---
+++ libs/data_structures/matrix/matrix.h
@@ -0,0 +1,15 @@
+#ifndef UNTITLED_MATRIX_H
+#define UNTITLED_MATRIX_H
+
+typedef struct matrix {
+    int **values; // элементы матрицы
+    int nRows; // количество рядов
+    int nCols; // количество столбцов
+} matrix;
+
+typedef struct position {
+    int rowIndex;
+    int colIndex;
+} position;
+
+#endif
