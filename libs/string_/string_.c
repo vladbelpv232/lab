a857f6e69944e0bac9d7dd64a5b3abedf88d4ab0
vladbelpv232 <vladbeloborodov@gmail.com>
2024-05-11 23:18:31 +0300

    add func strlen_

--- libs/string_/string_.c
+++ libs/string_/string_.c
@@ -1,2 +1,10 @@
+#include "string_.h"
 
+//возвращает количество символом в строке, не считая ноль-символ
+size_t strlen_(const char *begin) {
+    char *end = begin;
+    while (*end != '\0')
+        end++;
+    return end - begin;
+}
 
