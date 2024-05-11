e8243f7e28e7cb272c392a400601f007ecf2b3d0
vladbelpv232 <vladbeloborodov@gmail.com>
2024-05-11 23:29:53 +0300

    add func for find

--- libs/string_/string_.c
+++ libs/string_/string_.c
@@ -1,4 +1,5 @@
 #include "string_.h"
+#include <ctype.h>
 
 //возвращает количество символом в строке, не считая ноль-символ
 size_t strlen_(const char *begin) {
@@ -8,3 +9,48 @@ size_t strlen_(const char *begin) {
     return end - begin;
 }
 
+// возвращает указатель
+//на первый элемент с кодом ch, расположенным на ленте памяти между
+//адресами begin и end не включая end.
+char* find(char *begin, char *end, int ch) {
+    while (begin != end && *begin != ch)
+        begin++;
+    return begin;
+}
+
+//возвращает указатель на первый
+//символ, отличный от пробельных, расположенный на ленте памяти,
+// начиная с begin и заканчивая ноль-символом.
+char* findNonSpace(char *begin) {
+    while (*begin != '\0' && isspace(*begin)) {
+        begin++;
+    }
+    return begin;
+}
+
+//возвращает указатель на первый пробельный символ, расположенный
+//на ленте памяти начиная с адреса begin или на первый ноль-символ.
+char* findSpace(char *begin) {
+    while (*begin != '\0' && (*begin != ' ' && *begin != '\t' && *begin != '\r' && *begin != '\n')) {
+        begin++;
+    }
+    return begin;
+}
+
+//возвращает указатель на первый справа символ, отличный от пробельных,
+//расположенный на ленте памяти, начиная с rbegin и заканчивая rend.
+char* findNonSpaceReverse(char *rbegin, const char *rend) {
+    while (rbegin >= rend && (*rbegin == ' ' || *rbegin == '\t' || *rbegin == '\r' || *rbegin == '\n')) {
+        rbegin--;
+    }
+    return rbegin + 1;
+}
+
+//возвращает указатель на первый пробельный символ справа, расположенный
+//на ленте памяти, начиная с rbegin и заканчивая rend.
+char* findSpaceReverse(char *rbegin, const char *rend) {
+    while (rbegin != rend && !isspace(*rbegin)) {
+        rbegin--;
+    }
+    return rbegin;
+}
